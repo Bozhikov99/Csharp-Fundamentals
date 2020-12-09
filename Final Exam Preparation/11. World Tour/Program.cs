86e0c7cabf76d4ed5aec4ec3598c1b754937b5bb
Bozhikov99 <registracia103@abv.bg>
2020-12-09 18:28:32 +0200

    Uploaded my C# Fundamentals solutions

---
+++ Final Exam Preparation/11. World Tour/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace _11._World_Tour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string cmd = Console.ReadLine();
+            while (cmd!="Travel")
+            {
+                string[] tokens = cmd.Split(':', StringSplitOptions.RemoveEmptyEntries);
+                string command = tokens[0];
+                switch (command)
+                {
+                    case "Add Stop":
+                        int index = int.Parse(tokens[1]);
+                        string newStop = tokens[2];
+                        if (IsValidIndex(index, input))
+                        {
+                            input=input.Insert(index, newStop); 
+                        }
+                        break;
+
+                    case "Remove Stop":
+                        int start = int.Parse(tokens[1]);
+                        int end = int.Parse(tokens[2]);
+                        if (IsValidIndex(start, input)&&IsValidIndex(end, input))
+                        {
+                            input = input.Remove(start, end-start+1);
+                        }
+                        break;
+                    case "Switch":
+                        string oldString = tokens[1];
+                        string newString = tokens[2];
+                        if (input.Contains(oldString))
+                        {
+                            input = input.Replace(oldString, newString);
+                        }
+                        break;
+                    
+                    default:
+                        break;
+                }
+                Console.WriteLine(input);
+                cmd = Console.ReadLine();
+            }
+            Console.WriteLine($"Ready for world tour! Planned stops: {input}");
+        }
+        public static bool IsValidIndex(int index, string input)
+        {
+            if (index>=0&&index<input.Length)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Final Exam Preparation/13. Destination Mapper/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _13._Destination_Mapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(=|\/)(?<location>[A-Z][A-Za-z]{2,})\1";
+            MatchCollection matches = Regex.Matches(input, pattern);
+            int travelPoints = 0;
+            Console.Write("Destinations:");
+            for (int i = 0; i < matches.Count; i++)
+            {
+                if (i == matches.Count - 1)
+                {
+                    Console.Write($" {matches[i].Groups["location"]}");
+                }
+                else
+                {
+                    Console.Write($" {matches[i].Groups["location"]},");
+                }
+                travelPoints += matches[i].Groups["location"].Length;
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+}
