60502af1f88fc9e2034b4f7179bcf370db063bac
Bozhikov99 <registracia103@abv.bg>
2021-03-04 16:14:49 +0200

    Adding my Basic Web Project.

---
+++ 11. Basic Web Project/MyPhoneBook/Models/Contact.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPhoneBook.Models
+{
+    public class Contact
+    {
+        [Required(ErrorMessage = "Полето {0} е задължително!")]
+        [Display(Name="Име")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Полето {0} е задължително!")]
+        [StringLength(15, MinimumLength =5,ErrorMessage ="Дължината на полето {0} трябва да е между {2} и {1}.")]
+        [Display(Name="Номер")]
+        public string Number { get; set; }
+    }
+}
